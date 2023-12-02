54b6b80cfabe8ad753d2532b2db43571a4be5b72
sena@s73.work <Sergey>
2023-12-02 09:55:10 +0100

    test by capturing stdout

--- gtest_print_ip.cpp
+++ gtest_print_ip.cpp
@@ -5,55 +5,79 @@
 #include <new>
 #include <stdexcept>
 #include <string>
+#include <tuple>
 #include <vector>
 
 namespace {
 
+   // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+  TEST(Google_test, test_sizes) {
+    {
+      EXPECT_EQ(maxuint8, 255);
+      EXPECT_EQ(bytesize, 8);
+    }
+  }
+
   TEST(Google_test, test_integral) {
     {
-      std::stringstream ss;
-      os_ip(int8_t{-1}, ss);
-      EXPECT_EQ(ss.str(), "255");
+      testing::internal::CaptureStdout();
+      print_ip(int8_t{-1});
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "255\n");
     }
     {
-      std::stringstream ss;
-      os_ip( int16_t{0}, ss );
-      EXPECT_EQ(ss.str(), "0.0");
+      testing::internal::CaptureStdout();
+      print_ip( int16_t{0});
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "0.0\n");
     }
     {
-      std::stringstream ss;
-      os_ip( int32_t{2130706433}, ss );
-      EXPECT_EQ(ss.str(), "127.0.0.1");
+      testing::internal::CaptureStdout();
+      print_ip( int32_t{2130706433} );
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "127.0.0.1\n");
     }
     {
-      std::stringstream ss;
-      os_ip( int64_t{8875824491850138409}, ss);
-      EXPECT_EQ(ss.str(), "123.45.67.89.101.112.131.41");
+      testing::internal::CaptureStdout();
+      print_ip( int64_t{8875824491850138409});
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "123.45.67.89.101.112.131.41\n");
     }
   }
 
   TEST(Google_test, test_string) {
     {
-      std::stringstream ss;
-      os_ip(std::string{"Hello, World!"}, ss);
-      EXPECT_EQ(ss.str(), "Hello, World!");
+      testing::internal::CaptureStdout();
+      print_ip(std::string{"Hello, World!"});
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "Hello, World!\n");
     }
   }
 
 
   TEST(Google_test, test_vector) {
     {
-      std::stringstream ss;
-      os_ip( std::vector<int>{100, 200, 300, 400}, ss );
-      EXPECT_EQ(ss.str(), "100.200.300.400");
+      testing::internal::CaptureStdout();
+      print_ip( std::vector<int>{100, 200, 300, 400} );
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "100.200.300.400\n");
     }
   }
 
   TEST(Google_test, test_list) {
     {
-      std::stringstream ss;
-      os_ip( std::list<short>{400, 300, 200, 100}, ss );
-      EXPECT_EQ(ss.str(), "400.300.200.100");
+      testing::internal::CaptureStdout();
+      print_ip( std::list<short>{400, 300, 200, 100} );
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "400.300.200.100\n");
     }
   }
+
+
+  TEST(Google_test, test_tuple) {
+    {
+      testing::internal::CaptureStdout();
+      print_ip( std::make_tuple<short,short,short,short>(21, 22, 23, 24) );
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "21.22.23.24\n");
+    }
+    {
+      testing::internal::CaptureStdout();
+      print_ip( std::make_tuple("one","two","three") );
+      EXPECT_EQ(testing::internal::GetCapturedStdout(), "one.two.three\n");
+    }
+  }
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 }
